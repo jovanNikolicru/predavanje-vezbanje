9652ead9c73eb9263d3f43f0dc899fc7f18e5c1e
Jovan Nikolic <jovannikolic@gmail.com>
2021-12-28 19:50:17 +0100

    Array prin helper method

--- src/vezbanje/zadatak1.java
+++ src/vezbanje/zadatak1.java
@@ -3,8 +3,14 @@ package vezbanje;
 public class zadatak1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int [] niz = {1,2,3,4,5};
+		
+		stampaj(niz);
 
+	}public static void stampaj(int [] niz) {
+		int i;
+		for(i=0;i<niz.length;i++)
+		System.out.println(niz[i]);
 	}
 
 }
