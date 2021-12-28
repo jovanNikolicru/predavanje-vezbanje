bb7f76ee887a411b1b1753868ccdf82e6d087fbc
Jovan Nikolic <jovannikolic@gmail.com>
2021-12-28 20:05:55 +0100

    Add printReverse method for arrary

--- src/p28_12_2021/HelperMethods.java
+++ src/p28_12_2021/HelperMethods.java
@@ -6,11 +6,16 @@ public class HelperMethods {
 	int [] niz = {1,2,3,4,5};
 		
 		stampaj(niz);
-
+printReverse(niz);
 	}public static void stampaj(int [] niz) {
 		int i;
 		for(i=0;i<niz.length;i++)
-		System.out.println(niz[i]);
+		System.out.print(niz[i]);
+	}public static void printReverse (int [] niz) {
+		int i;
+		for (i=niz.length - 1; i>=0;i--) {
+			System.out.print(niz[i]);
+		}
 	}
 
 }
